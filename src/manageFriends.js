b77dc4cee57f591c6fbbff2d95d29c0f54b1acd3
Alessandra Chardon <achardon@ucla.edu>
2022-04-12 11:34:21 -0400

    assignment complete

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,14 @@
-export function manageFriends(state, action) {
-  // your code here
+const initialState = { friends: [] }
+
+export function manageFriends(state = initialState, action) {
+  switch (action.type) {
+    case "friends/add":
+      const newArray = [...state.friends, action.payload]
+      return { friends: newArray};
+    case "friends/remove":
+      const updatedArray = state.friends.filter(n => n.id !== action.payload)
+      return { friends: updatedArray};
+    default: 
+      return state
+  }
 }
